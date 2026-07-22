efb08bf691926a372d2d8ed5288cc3b83fded43f
mujtaba <hmmujtaba87@gmail.com>
2026-07-22 21:48:06 +0500

    Updated  url files

--- config/urls.py
+++ config/urls.py
@@ -14,19 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.auth.views import LogoutView
+from django.urls import include, path
+
 from .views import SignupView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('trip.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup/', SignupView.as_view(), name = 'signup'),
+    path("admin/", admin.site.urls),
 
+    path("", include("trip.urls")),
+
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/signup/", SignupView.as_view(), name="signup"),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
--- trip/admin.py
+++ trip/admin.py
@@ -1,6 +1,41 @@
 from django.contrib import admin
-from .models import Trip, Note
 
-# Register your models here.
-admin.site.register(Trip)
-admin.site.register(Note)
+from .models import Note, Trip
+
+
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = (
+        "city",
+        "country",
+        "owner",
+        "start_date",
+        "end_date",
+    )
+    list_filter = (
+        "country",
+        "start_date",
+    )
+    search_fields = (
+        "city",
+        "owner__username",
+    )
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "trip",
+        "type",
+        "rating",
+    )
+    list_filter = (
+        "type",
+        "rating",
+    )
+    search_fields = (
+        "name",
+        "trip__city",
+        "trip__owner__username",
+    )
