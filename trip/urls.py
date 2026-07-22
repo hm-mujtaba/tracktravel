959821b016de9b7c970bf97d4ca447904d6bd314
mujtaba <hmmujtaba87@gmail.com>
2026-07-22 22:09:45 +0500

    remained untraacked

--- trip/urls.py
+++ trip/urls.py
@@ -1,31 +1,35 @@
 from django.urls import path
+
 from .views import (
     HomeView,
-    trips_list,
-    TripCreateView,
-    TripDetailView,
+    NoteCreateView,
+    NoteDeleteView,
     NoteDetailView,
     NoteListView,
-    NoteCreateView,
     NoteUpdateView,
-    NoteDeleteView,
+    TripCreateView,
     TripDeleteView,
+    TripDetailView,
     TripUpdateView,
+    trips_list,
 )
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    path("", HomeView.as_view(), name="home"),
 
-    path('dashboard/', trips_list, name='trip-list'),
+    # Dashboard
+    path("dashboard/", trips_list, name="trip-list"),
 
-    path('dashboard/note/', NoteListView.as_view(), name='note-list'),
-    path('dashboard/note/create/', NoteCreateView.as_view(), name='note-create'),
-    path('dashboard/note/<int:pk>/', NoteDetailView.as_view(), name='note-detail'),
-    path('dashboard/note/<int:pk>/update/', NoteUpdateView.as_view(), name='note-update'),
-    path('dashboard/note/<int:pk>/delete/', NoteDeleteView.as_view(), name='note-delete'),
+    # Trips
+    path("dashboard/trips/create/", TripCreateView.as_view(), name="trip-create"),
+    path("dashboard/trips/<int:pk>/", TripDetailView.as_view(), name="trip-detail"),
+    path("dashboard/trips/<int:pk>/update/", TripUpdateView.as_view(), name="trip-update"),
+    path("dashboard/trips/<int:pk>/delete/", TripDeleteView.as_view(), name="trip-delete"),
 
-    path('dashboard/trip/create/', TripCreateView.as_view(), name='trip-create'),
-    path('dashboard/trip/<int:pk>/', TripDetailView.as_view(), name='trip-detail'),
-    path('dashboard/trip/<int:pk>/update/', TripUpdateView.as_view(), name='trip-update'),
-    path('dashboard/trip/<int:pk>/delete/', TripDeleteView.as_view(), name='trip-delete'),
+    # Notes
+    path("dashboard/notes/", NoteListView.as_view(), name="note-list"),
+    path("dashboard/notes/create/", NoteCreateView.as_view(), name="note-create"),
+    path("dashboard/notes/<int:pk>/", NoteDetailView.as_view(), name="note-detail"),
+    path("dashboard/notes/<int:pk>/update/", NoteUpdateView.as_view(), name="note-update"),
+    path("dashboard/notes/<int:pk>/delete/", NoteDeleteView.as_view(), name="note-delete"),
 ]
